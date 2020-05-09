f2555187944d5d027f1a13182f4db5ddca70a695
Ummul Khair Fatima <hoory1020@gmail.com>
2020-05-09 17:18:13 +0300

    Update index.js

--- src/components/FileUploadButton/index.js
+++ src/components/FileUploadButton/index.js
@@ -29,7 +29,7 @@ const FileUploadButton = () => {
   const onClickHandler = async() => {
     const data = new FormData() 
     data.append('file', selectedFile)
-    const res = await axios.post("http://crosskeyspringrest-env.eba-2nhmp3dm.us-east-1.elasticbeanstalk.com/upload", data)
+    const res = await axios.post("https://crosskeyspringrest-env.eba-2nhmp3dm.us-east-1.elasticbeanstalk.com/upload", data)
     setprospectsData(res.data)
   }
   
